449e546783e7f89c88fac146582f35493b9ea36b
sonnguthienngu <sonnguthienngu@gmail.com>
2025-06-04 14:14:03 +0700

    aaa

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -116,6 +116,7 @@ const Header = () => {
               >
                 For Sellers
               </Link>
+              
               <div className="flex flex-col space-y-2 pt-4">
                 <Link to="/auth/login" onClick={() => setIsMenuOpen(false)}>
                   <Button variant="outline" className="w-full">Sign In</Button>
@@ -125,7 +126,7 @@ const Header = () => {
                     setIsMenuOpen(false);
                     handleGetStarted();
                   }}
-                  className="w-full bg-teal-600 hover:bg-teal-700"
+                  className="w-full bg-teal-600 hover:bg-teal-700 text-white"
                 >
                   Get Started
                 </Button>
